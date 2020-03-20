d236278f5638f65e507bb9a6b402d565e5945089
Marko Bezic <marko.bezic@gmail.com>
2020-03-20 11:39:46 -0400

    need to debug

--- employee_directory_v1/app.js
+++ employee_directory_v1/app.js
@@ -1,51 +1,80 @@
-const cards = document.getElementsByClassName('cards');
+//global variables
+let employees = [];
+const urlAPI = `https://randomuser.me/api/?results=12` //`https://fsjs-public-api-backup.herokuapp.com/api/`
+const gridContainer = document.querySelector(".grid-container");
+const overlay = document.querySelector(".overlay");
+const modalContainer = document.querySelector(".modal-content");
+const modalClose = document.querySelector(".modal-close");
+
+console.log(urlAPI);
 
 //Use fetch to get and display 12 random users from  THE RANDOM USER GENERATOR API
-    //Using photos and information that the API provides, you'll display 12 users, along with some basic information:
-        //image
-        //First and Last Name
-        //Email
-        //City
+    //display 12 users, along with some basic information
 
-const users = fetch('https://fsjs-public-api-backup.herokuapp.com/api/')
-    .then(response => response.json())
-    .then(data => generateHTML(data));
+fetch(urlAPI)
+    .then(res => res.json())
+    .then(res => res.results)
+    .then(displayEmployees)
+    .catch(err => console.log(err))
 
-        //Generate the markup for each profile//
-function generateHTML(data){
-    data.map( card => {
-    cards.innerHTML = `
-        <img src=${card.picture.thumbnail}>
-        <h3>${card.name.first} ${card.name.last}</h3>
-        <p>${card.email}</p>
-        <p>${card.location.city}</p>
-    `
+function displayEmployees(employeeData) { 
+    employees = employeeData;
+
+    //store the employee HTML as we create it
+    let employeeHTML = '';
+
+    //loop through each employee and create HTML markup
+    employees.forEach((employee, index) => {
+        let name = employee.name;
+        let email = employee.email;
+        let city = employee.location.city;
+        let picture = employees.picture;
+
+        //template literals
+        employeeHTML +=`
+        <div class="card" data-index="${index}">
+        <img class="avatar" src="${picture.large}">
+        <div class="text-container">
+            <h2 class="name">${name.first} ${name.last}</h2>
+            <p class="email">${email}</p>
+            <p class="address">${city}</p>
+        </div>
+    </div>
+        `
     });
+    gridContainer.innerHTML = employeeHTML;
 }
 
+//Create a modal window that will pop up when any part of the user's row is clicked. 
 
-//Create a modal window that will pop up when any part of the user's row is clicked. The following details should display in the modal window:
-    //Image
-    //Name
-    //Email
-    //Cell Number
-    //Detailed Address, including street name and number, city, state, and postcode
-    //Birthdate
+function diplayModal(index) {
+    let { name, dob, phone, email, location: { city, street, state, postcode}, picture } = employee[index];
 
-//Structure and style the user directory so that it roughly matches the provide mockup
-    //Display the users in a grid or table
-    //Add a hover state to the employee cards
-    //Make sure there's a way to close the modal window
+    let date = new Date(dob.date);
 
-//Extra credit
-    //Add a way to filter the directory by name. To do this, you'll need to request a random 
-    //user nationality that will only return data in the English alphabet. Note you don't have 
-    //to rely on the API to return search results. You'll need to write functionality that 
-    //filters results once they're already on the page.
-   
-    //Add a way to move back and forth between employee detail window when the modal window is open
+    const modalHTML = `
+        <img class="avatar" src"${picture.large}" >
+        <div class="text-container">
+            <h2 class="name">${name.first} ${name.last}</h2>
+            <p class="email">${email}</p>
+            <hr>
+            <p>${phone}</p>
+            <p class="address">${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+    `;
 
+    overlay.classList.remove("hidden");
+    modalContainer.innerHTML = modalHTML;
+}
 
-    //////////Left trying to figure out who to pull the date from the api create objects with only 
-    ////the 4 parameters you need. I'm thinking I'll need to sit down and consider the step by step 
-    ////of how to add this to my page. Maybe lookc those mock up images too :)
+gridContainer.addEventListener('click', e => {
+    if(e.target !== gridContainer) {
+        const card = e.target.closest(".card");
+        const index = card.getAttribute(data-index);
+
+        displayModal(index);
+    }
+});
+
+modalClose.addEventListener('click', () => {
+    overlay.classList.add("hidden");
+});
