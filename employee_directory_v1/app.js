104200020e5c7c9fa1ef6fa0c4f7398dafaa456e
Marko Bezic <marko.bezic@gmail.com>
2020-03-23 10:17:02 -0400

    debug complete

--- employee_directory_v1/app.js
+++ employee_directory_v1/app.js
@@ -28,7 +28,7 @@ function displayEmployees(employeeData) {
         let name = employee.name;
         let email = employee.email;
         let city = employee.location.city;
-        let picture = employees.picture;
+        let picture = employee.picture;
 
         //template literals
         employeeHTML +=`
@@ -47,19 +47,21 @@ function displayEmployees(employeeData) {
 
 //Create a modal window that will pop up when any part of the user's row is clicked. 
 
-function diplayModal(index) {
-    let { name, dob, phone, email, location: { city, street, state, postcode}, picture } = employee[index];
+function displayModal(index) {
+    let { name, dob, phone, email, location: { city, street, state, postcode}, picture } = employees[index];
 
     let date = new Date(dob.date);
 
     const modalHTML = `
-        <img class="avatar" src"${picture.large}" >
+        <img class="avatar" src="${picture.large}" >
         <div class="text-container">
             <h2 class="name">${name.first} ${name.last}</h2>
             <p class="email">${email}</p>
             <hr>
-            <p>${phone}</p>
-            <p class="address">${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+            <p class="phone">${phone}</p>
+            <p class="address">${street.number} ${street.name}, ${state} ${postcode}</p>
+            <p class="birthday">Birthday: ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+        </div>
     `;
 
     overlay.classList.remove("hidden");
@@ -69,7 +71,7 @@ function diplayModal(index) {
 gridContainer.addEventListener('click', e => {
     if(e.target !== gridContainer) {
         const card = e.target.closest(".card");
-        const index = card.getAttribute(data-index);
+        const index = card.getAttribute('data-index');
 
         displayModal(index);
     }
